b419c04ec63b24a12112361260fd69f21fa8d07d
AlgoClassWork <kyxec.play@gmail.com>
2025-05-27 10:04:39 +0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const quizData = [
+        { 
+            question : 'Какого цвета чернокожие?',
+            options: ['Белые', 'Черные', 'Зеленые', 'Голубые'],
+            correctAnswer: 'Черные'
+        },
+        { 
+            question : 'Какая река самая длинная?',
+            options: ['Нил', 'Амазонка', 'Янцзы', 'Миссисипи'],
+            correctAnswer: 'Нил'
+        },
+        { 
+            question : 'В каком году началась Вторая мировая война?',
+            options: ['1938', '1941', '1940', '1939'],
+            correctAnswer: '1939'
+        },
+    ]
+
+
+})
